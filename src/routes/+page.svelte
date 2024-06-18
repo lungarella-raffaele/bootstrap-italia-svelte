06bca75d20dce74dc447721f9768382558c9448d
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-06-18 11:49:13 +0200

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Bootstrap-Italia with Svelte" />
+</svelte:head>
+
+<section class="content">
+	<div class="mb-5">
+		<h1>Bootstrap Italia Test for Svelte</h1>
+	</div>
+<div class="form-group">
+    <label for="exampleInputText">Campo di tipo testuale</label>
+    <input type="text" class="form-control" id="exampleInputText">
+  </div>	
+</section>
+
+<style>
+	.content{
+		padding: 20rem;
+	}
+</style>
